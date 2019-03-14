572a8bd1f22e8fcbe8605a4c0bee6b07cb06942c
Lam Nhan <lamnhan92@gmail.com>
2019-03-14 16:37:12 +0700

    add more query

--- src/lib/sheets.ts
+++ src/lib/sheets.ts
@@ -1,6 +1,6 @@
 import { AddonRoutesOptions, RoutingErrors, RouteHandler } from '@sheetbase/core-server';
 
-import { Options, Extendable, Intergration, Filter, Database } from './types';
+import { Options, Extendable, Intergration, Filter, Query, AdvancedFilter, Database } from './types';
 import { SecurityService } from './security';
 import { DataService } from './data';
 
@@ -52,17 +52,85 @@ export class SheetsService {
     }
 
     query<Item>(sheetName: string, filter: Filter) {
-        if (!(filter instanceof Function)) {
-            let { where, equal } = filter as any;
-            if (!where) { // pass in an object, format: { where: equal }
+        let advancedFilter: AdvancedFilter;
+        if (filter instanceof Function) {
+            advancedFilter = filter;
+        } else {
+            // where/equal shorthand: { where: equal }
+            let { where, equal } = filter as Query;
+            if (!where) {
                 where = Object.keys(filter)[0];
                 equal = filter[where];
             }
-            filter = item => {
-                return item[where] === equal;
-            };
+            // build advanced filter
+            const {
+                exists,
+                contains,
+                lt, lte,
+                gt, gte,
+                childExists,
+                childEqual,
+            } = filter as Query;
+            if (!!equal) { // where/equal
+                advancedFilter = item => (!!item[where] && item[where] === equal);
+            } else if (typeof exists === 'boolean') { // where/exists/not exists
+                advancedFilter = item => (!!exists ? !!item[where] : !item[where]);
+            } else if (!!contains) { // where/contains
+                advancedFilter = item => (
+                    !!item[where] &&
+                    typeof item[where] === 'string' &&
+                    item[where].indexOf(contains) > -1
+                );
+            } else if (!!lt) { // where/less than
+                advancedFilter = item => (
+                    !!item[where] &&
+                    typeof item[where] === 'number' &&
+                    item[where] < lt
+                );
+            } else if (!!lte) { // where/less than or equal
+                advancedFilter = item => (
+                    !!item[where] &&
+                    typeof item[where] === 'number' &&
+                    item[where] <= lte
+                );
+            } else if (!!gt) { // where/greater than
+                advancedFilter = item => (
+                    !!item[where] &&
+                    typeof item[where] === 'number' &&
+                    item[where] > gt
+                );
+            } else if (!!gte) { // where/greater than or equal
+                advancedFilter = item => (
+                    !!item[where] &&
+                    typeof item[where] === 'number' &&
+                    item[where] >= gte
+                );
+            } else if (!!childExists) { // where/childExists
+                advancedFilter = item => {
+                    if (!!item[where]) {
+                        if (item[where] instanceof Array) {
+                            return (childExists === false) ?
+                                (item[where].indexOf(childExists) < 0) :
+                                (item[where].indexOf(childExists) > -1);
+                        } else if (item[where] instanceof Object) {
+                            const childKey = childExists.replace('!', '');
+                            const notExists = childExists.substr(0, 1) === '!';
+                            return notExists ? !item[where][childKey] : !!item[where][childKey];
+                        }
+                    }
+                    return false;
+                };
+            } else if (!!childEqual) { // where/childEqual
+                const [ childKey, childValue ] = childEqual.split('=').filter(Boolean);
+                advancedFilter = item => (
+                    !!item[where] &&
+                    item[where] instanceof Object &&
+                    !!item[where][childKey] &&
+                    item[where][childKey] === childValue
+                );
+            }
         }
-        return this.ref('/' + sheetName).query(filter as any) as Item[];
+        return this.ref('/' + sheetName).query(advancedFilter) as Item[];
     }
 
     item<Item>(sheetName: string, finder: string | Filter) {
--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,8 +1,17 @@
-export type Filter = {[field: string]: any} | Query | AdvancedFilter;
 export type AdvancedFilter = (item: any) => boolean;
+export type ShorthandEqual = {[field: string]: any};
+export type Filter = ShorthandEqual | Query | AdvancedFilter;
 export interface Query {
     where: string;
     equal?: any;
+    exists?: true;
+    contains?: string;
+    lt?: number;
+    lte?: number;
+    gt?: number;
+    gte?: number;
+    childExists?: any;
+    childEqual?: string;
 }
 
 export interface Intergration {
